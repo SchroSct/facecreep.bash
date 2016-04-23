cbfb85426bbc687b203b4f6f1761f6e6b32643a9
SchroSct <clls760@gmail.com>
2016-04-23 09:47:49 -0700

    Update facecreep-full.bash

--- facecreep-full.bash
+++ facecreep-full.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
-lockdir="/tmp/facecreep.lock/"
+lockdir="/tmp/facecreep-full.lock/"
 if mkdir "$lockdir"
 then
    echo "Lock created, running $0"
 else
-   echo "facecreep is already running, please wait until completion."
+   echo "facecreep-full is already running, please wait until completion."
    exit 1
 fi
 ua="Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
 site="https://m.facebook.com"
-if ! read directory < ~/.config/facecreep.rc
+if ! read directory < ~/.config/facecreep-full.rc
 then
-   echo ".config/facecreep.rc not set"
+   echo ".config/facecreep-full.rc not set"
    rmdir "$lockdir"
    exit 1
 fi
@@ -28,6 +28,7 @@ mapfile -t targets < <(cat facecreep.list.txt)
 email="${creds[0]}"
 password="${creds[1]}"
 uname="${creds[2]}"
+
 callpage()
 {
 curl -s -L -b "${directory}facecreep.txt" -A "$ua" "$1"
@@ -45,16 +46,68 @@ else
 curl -L -b "${directory}facecreep.txt" -A "$ua" -o "${name}" "$1"
 fi
 }
-while true
+
+creep()
+{
+if callpage "$site" | grep -q -i "$uname"
+then
+   echo "Already Logged In"
+else
+   login > /dev/null
+fi
+mapfile -t albums < <(callpage "${site}/${target}?v=photos" | sed -e 's/href="/\n/g'| grep -i "${target}/albums" | sed -e 's/".*//g')
+mapfile -O 3 -t albums < <(callpage "${site}/${target}/photos?psm=default&startindex=3" | sed -e 's/href="/\n/g'| grep -i "${target}/albums" | sed -e 's/".*//g')
+for album in "${albums[@]}"
 do
-   if callpage "$site" | grep -q -i "$uname"
+   echo "${album}"
+   alnum=$(echo "${album}" | sed -e 's/.*albums\///g' -e 's/\///g')
+   if [[ -d "${alnum}" ]]
    then
-      echo "Already Logged In"
+      echo "${alnum} exists, crawling."
    else
-      login > /dev/null
+      mkdir "${alnum}"
    fi
-   for target in "${targets[@]}"
+   if cd "${alnum}"
+   then
+   mapfile -t photos < <(callpage "${site}${album}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep "photo.php" | sed -e 's/.*\/photo.php/\/photo.php/g')
+   fphoto="${photos[1]}"
+   mapfile -t urls < <(callpage "${site}${fphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep ".jpg" | grep -v "quot" )
+   fname=$(echo "${urls[${ord}]}" | sed -e 's/.*\///g' -e 's/\?.*//g')
+   curld "${urls[${ord}]}"
+   nphoto=$(callpage "${site}${fphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep -A 2 replace-state | tac | grep -m 1 photo.php)
+   nname=""
+   lname="${fname}"
+   until [[ "${nname}" = "${fname}" ]] || [[ "${lname}" = "${nname}" ]]
    do
+      mapfile -t urls < <(callpage "${site}${nphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep ".jpg" | grep -v "quot" )
+      lname="${nname}"
+      nname=$(echo "${urls[${ord}]}" | sed -e 's/.*\///g' -e 's/\?.*//g')
+      curld "${urls[${ord}]}"
+      nphoto=$(callpage "${site}${nphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep -A 2 replace-state | tac | grep -m 1 photo.php)
+   done
+   if cd "${directory}${target}"
+   then
+      echo "Back in ${target} main directory"
+   else
+      echo "Could not enter ${directory}${target}"
+      rmdir "$lockdir"
+      exit 1
+   fi
+fi
+done
+
+}
+for target in "${targets[@]}"
+do
+   if cd "$directory"
+   then
+      echo "Inside $directory"
+   else
+      echo "Could not enter $directory"
+      rmdir "$lockdir"
+      exit 1
+   fi
+
    if [[ -d "${target}" ]]
    then
       echo "${target} exists, crawling."
@@ -63,37 +116,15 @@ do
    fi
    if cd "${target}"
    then
-      mapfile -t albums < <(callpage "${site}/${target}?v=photos" | sed -e 's/"/\n/g' -e 's/\\//g' | sed -e 's/href="/\n/g'| grep -i "${target}/albums" | sed -e 's/".*//g' )
-      mapfile -O 3 -t albums < <(callpage "${site}/${target}/photos?psm=default&startindex=3" | sed -e 's/"/\n/g' -e 's/\\//g' | sed -e 's/href="/\n/g'| grep -i "${target}/albums" | sed -e 's/".*//g' )
-      for album in "${albums[@]}"
-      do
-         echo "${album}"
-         alnum=$(echo "${album}" | sed -e 's/.*albums\///g' -e 's/\///g')
-         if [[ -d "${alnum}" ]]
-         then
-            echo "${alnum} exists, crawling."
-         else
-            mkdir "${alnum}"
-         fi
-         if cd "${alnum}"
-         then
-            mapfile -t photos < <(callpage "${site}${album}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep "photo.php" | sed -e 's/.*\/photo.php/\/photo.php/g' )
-            for photo in "${photos[@]}"
-            do
-               mapfile -t urls < <(callpage "${site}${photo}" | sed -e 's/"/\n/g' -e 's/\\//g' |grep -A 3 init| grep -i n.jpg | sed -e 's/.*url(\&quot\;https/https/g' -e 's/\&quot\;).*//g' )
-               for i in "${urls[@]}"
-               do
-                  curld "${i}"
-               done
-            done
-         fi
-         cd "${directory}${target}"
-      done
+      ord=1
+      creep "$target"
+      ord=2
+      creep "$target"
+      ord=3
+      creep "$target"
+   else
+      echo "$0 FAILED"
    fi
-   cd "$directory"
-   done
-   let random=$RANDOM%360
-   let sleepy=45+$random
-   echo "sleeping $sleepy on $(date)"
-   sleep $sleepy
 done
+
+rmdir "$lockdir"
