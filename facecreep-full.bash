1eacad46e673c0e887349ee32a5b548be2c888d1
SchroSct <clls760@gmail.com>
2016-04-23 06:57:15 -0700

    Update facecreep-full.bash
    Reads directory from a .config/facecreep-full.rc file.  Some other changes, like now we bruteforce the url we use for the picture, because FB seems to rotate shit on me, and idk how to distinguish when they do.

--- facecreep-full.bash
+++ facecreep-full.bash
@@ -1,26 +1,31 @@
 #!/bin/bash
-if [[ -a /tmp/facecreep.pid ]]
+if mkdir "/tmp/facecreep-full.lock/"
 then
-   echo "Facecreep is already running, please wait until completion."
+   echo "Lock created, running $0"
+else
+   echo "facecreep-full is already running, please wait until completion."
    exit 1
 fi
-touch /tmp/facecreep.pid
 ua="Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
 site="https://m.facebook.com"
-directory=""
-if cd "$directory"
+if ! read directory < ~/.config/facecreep-full.rc
 then
-   echo "Inside $directory"
-else
-   echo "Could not enter $directory"
+   echo ".config/facecreep-full.rc not set"
    exit 1
 fi
+   if cd "$directory"
+   then
+      echo "Inside $directory"
+   else
+      echo "Could not enter $directory"
+      exit 1
+   fi
 mapfile -t creds < <(cat facecreep.rc.txt)
 mapfile -t targets < <(cat facecreep.list.txt)
 email="${creds[0]}"
 password="${creds[1]}"
 uname="${creds[2]}"
-
+ 
 callpage()
 {
 curl -s -L -b "${directory}facecreep.txt" -A "$ua" "$1"
@@ -38,7 +43,7 @@ else
 curl -L -b "${directory}facecreep.txt" -A "$ua" -o "${name}" "$1"
 fi
 }
-
+ 
 creep()
 {
 if callpage "$site" | grep -q -i "$uname"
@@ -64,8 +69,8 @@ do
    mapfile -t photos < <(callpage "${site}${album}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep "photo.php" | sed -e 's/.*\/photo.php/\/photo.php/g')
    fphoto="${photos[1]}"
    mapfile -t urls < <(callpage "${site}${fphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep ".jpg" | grep -v "quot" )
-   fname=$(echo "${urls[1]}" | sed -e 's/.*\///g' -e 's/\?.*//g')
-   curld "${urls[1]}"
+   fname=$(echo "${urls[${ord}]}" | sed -e 's/.*\///g' -e 's/\?.*//g')
+   curld "${urls[${ord}]}"
    nphoto=$(callpage "${site}${fphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep -A 2 replace-state | tac | grep -m 1 photo.php)
    nname=""
    lname="${fname}"
@@ -73,8 +78,8 @@ do
    do
       mapfile -t urls < <(callpage "${site}${nphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep ".jpg" | grep -v "quot" )
       lname="${nname}"
-      nname=$(echo "${urls[1]}" | sed -e 's/.*\///g' -e 's/\?.*//g')
-      curld "${urls[1]}"
+      nname=$(echo "${urls[${ord}]}" | sed -e 's/.*\///g' -e 's/\?.*//g')
+      curld "${urls[${ord}]}"
       nphoto=$(callpage "${site}${nphoto}" | sed -e 's/"/\n/g' -e 's/\\//g' | grep -A 2 replace-state | tac | grep -m 1 photo.php)
    done
    if cd "${directory}${target}"
@@ -106,10 +111,15 @@ do
    fi
    if cd "${target}"
    then
+      ord=1
+      creep "$target"
+      ord=2
+      creep "$target"
+      ord=3
       creep "$target"
    else
-      echo "FACECREEP FAILED"
+      echo "$0 FAILED"
    fi
 done
 
-rm /tmp/facecreep.pid
+rmdir "/tmp/facecreep-full.lock/"
